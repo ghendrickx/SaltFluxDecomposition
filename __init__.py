08035061dcadc57cd6c84d8c41e2c405c2adacd3
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-10-27 14:53:53 +0200

    ADD: repository-files.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+`SaltFluxDecomposition` is a `NumPy`-based repository to perform a salt flux decomposition.
+
+Author: Gijs G. Hendrickx
+"""
+__all__ = ['src']
+
+__version__ = '1.0'
+__author__ = 'Gijs G. Hendrickx'
+__description__ = '`NumPy`-based salt flux decomposition'
